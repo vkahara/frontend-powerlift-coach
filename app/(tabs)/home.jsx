773c2584fef9a8ce2a98e3dc95a767c29cb68685
eemeligustafsson <eemeli.gustafsson@hotmail.com>
2024-06-10 19:06:54 +0300

    styling

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
+
       <Text>home</Text>
     </View>
   )
 }
 
 export default Home
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#2D5563',
+    },
+    background: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      height: 300,
+    },
+    button: {
+      padding: 15,
+      alignItems: 'center',
+      borderRadius: 5,
+    },
+    text: {
+      backgroundColor: 'transparent',
+      fontSize: 15,
+      color: '#fff',
+    },
+  });
