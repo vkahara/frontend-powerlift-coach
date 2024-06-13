a3fd23392e44c05275fc3f7f7412d3b110f8e729
vkahara <valtterikahara@gmail.com>
2024-06-13 10:37:57 +0300

    created workout tab

---
+++ app/(tabs)/workout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useWeekData } from "../../fetching/week";
+
+const Workout = () => {
+  return (
+    <View>
+      <Text>workou workout</Text>
+    </View>
+  );
+};
+
+export default Workout;
