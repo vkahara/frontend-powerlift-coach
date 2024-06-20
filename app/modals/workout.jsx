33e3fc2981a0e2173f8275a60a1669e331c255a8
vkahara <valtterikahara@gmail.com>
2024-06-20 12:43:32 +0300

    exercisebox and workout styles

--- app/modals/workout.jsx
+++ app/modals/workout.jsx
@@ -1,22 +1,47 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { useWeekData } from "../../contexts/WeekDataContext";
-import ExerciseBox from "./ExerciseBox";
+import ExerciseBox from "../../components/ExerciseBox";
 
 const Workout = () => {
   const { weekData, selectedDay } = useWeekData();
   const exercises = weekData.days[selectedDay].exercises;
 
   return (
-    <View>
-      <Text>Päivä numero {selectedDay + 1}</Text>
-      {exercises.map((exercise, index) => (
-        <View key={index}>
-          <ExerciseBox exercise={exercise} />
-        </View>
-      ))}
+    <View style={styles.container}>
+      <LinearGradient colors={["#2D5563", "#162A32"]} style={styles.background} />
+      <Text style={styles.dayText}>Päivä numero {selectedDay + 1}</Text>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        {exercises.map((exercise, index) => (
+          <ExerciseBox key={index} exercise={exercise} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  dayText: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '800',
+    fontSize: 20,
+    lineHeight: 24,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  scrollViewContent: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+});
 
 export default Workout;
